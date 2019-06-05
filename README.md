432608aa7b759842d89f9be649bd22ef6a92dfca
AJIOB <aleksandr9809@gmail.com>
2019-06-05 23:44:56 +0300

    Read-write commands were implemented

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Read SIM Phone Book from Quectel M66 over serial port with AT commands.
 Arguments:
 * Serial port file descriptor (`/dev/ttyUSB0`, for example)
 * Serial port baudrate (`9600`, for example)
+* Current working mode:
+    * `read`: read modem contact book to file & create write commands
+    * `write`: write modem contact book from file to SIM
--- phonebook_reader.sh
+++ phonebook_reader.sh
@@ -3,9 +3,12 @@
 SCRIPT_ROOT=$(dirname $0)
 MODEM_IO_FILE=$1
 MODEM_IO_BAUD=$2
+MODEM_IO_MODE=$3
 SERIAL_TOOL_ROOT="$SCRIPT_ROOT/serial_tool"
 SERIAL_TOOL_BIN="$SERIAL_TOOL_ROOT/out/serial_tool"
 MODEM_OUT_ENCODING="UCS2"
+MODEM_CONTACTS_IN_BLOCK=10
+CONTACT_OUT_FILE="out.txt"
 
 function make_tool {
     if [ ! -x "$SERIAL_TOOL_BIN" ]; then
@@ -56,13 +59,42 @@ function modem_read_info {
         echo "$out"
         exit 1
     fi
-    records=`echo "$out" | grep "+CPBS" | sed 's/^.*,\([0-9]\+\),.*$/\1/'`
+    record_num=`echo "$out" | grep "+CPBS" | sed 's/^.*,\([0-9]\+\),.*$/\1/'`
+    echo "Used $record_num records"
 
-    echo "Used $records records"
+    # read contacts
+    for (( i=1; i<=$record_num; i+=$MODEM_CONTACTS_IN_BLOCK ))
+    do
+        local out=`modem_run_with_check $(($MODEM_CONTACTS_IN_BLOCK / 2)) "AT+CPBR=$i,$(($i + $MODEM_CONTACTS_IN_BLOCK - 1))"`
+        if [ -n $? ]; then
+            echo "$out"
+            exit 1
+        fi
 
-    # TODO: implement
+        contacts=`echo "$out" | grep "+CPBR" | sed 's/+CPBR:/AT+CPBW=/'` >> $CONTACT_OUT_FILE
+    done
+}
+
+function modem_write_info {
+    # Set output encoding
+    modem_run_with_check 1 "AT+CSCS=\"$MODEM_OUT_ENCODING\""
+
+    # write contacts
+    while IFS= read -r line
+    do
+        local out=`modem_run_with_check 1 "$line"`
+        if [ -n $? ]; then
+            echo "$out"
+            exit 1
+        fi
+    done < "$CONTACT_OUT_FILE"
 }
 
 make_tool
 modem_work_check
-modem_read_info
+
+if [ "read"="$MODEM_IO_MODE" ]; then
+    modem_read_info
+elif [ "write"="$MODEM_IO_MODE" ]; then
+    modem_write_info
+fi
